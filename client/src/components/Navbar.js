a9e38bcd0e80ccbaa5ec66a4269a38f68cef9c2c
Melody Simwa <simwamelody@gmail.com>
2025-04-24 15:27:45 +0300

    add navbar.js code

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth } from './AuthContext';
+
+function Navbar() {
+  const { user, logout } = useAuth();
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">Health Information System</Link>
+      </div>
+      
+      <div className="navbar-links">
+        {user ? (
+          <>
+            <Link to="/register/doctor" className="nav-link">
+               Doctor Registration
+           </Link>
+            <Link to="/clients" className="nav-link">
+              Clients
+            </Link>
+            <Link to="/programs" className="nav-link">
+              Programs
+            </Link>
+            <button onClick={logout} className="btn-logout">
+              Logout
+            </button>
+          </>
+        ) : (
+          <Link to="/login" className="nav-link">
+            Login
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
+
+
+
